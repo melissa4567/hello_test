63ee5ee5ba3416fcd1dc3956f7c2f41238d2642d
melissa4567 <meljade175@gmail.com>
2025-10-21 09:49:55 -0600

    firstpython code

---
+++ hello.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+7 number = 5
+8 result = factorial(number)
+9 print(f"The factorial of {number} is {result}.")
+
